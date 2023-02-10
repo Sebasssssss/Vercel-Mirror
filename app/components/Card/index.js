3f48d8d498522c32d4bba1d29c1e8a37f982e68a
Sebasssssss <rodriguez.sebastian9714@gmail.com>
2023-02-10 16:44:48 -0300

    Added default props to Cards

--- app/components/Card/index.js
+++ app/components/Card/index.js
@@ -1,24 +1,27 @@
 import { GitBranch, Github } from '@geist-ui/icons'
 import Image from 'next/image'
-import avatar from '../../../public/avatar.jpg'
 
-export default function Card() {
+export default function Card({
+  image,
+  title = 'Example',
+  source = 'deploy.vercel.app',
+  commit = 'first commit',
+  branch = 'main'
+}) {
   return (
     <div className="w-full h-52 bg-black border border-[#333] hover:border-white hover:border-2 rounded-lg p-4 mt-4">
       <div className="inline-flex items-center gap-2">
-        <div className="w-8 h-8 rounded-full">
-          <Image alt="Avatar" src={avatar} />
-        </div>
+        <Image alt={title} src={image} className="w-8 h-8 rounded-full" />
         <div>
-          <h1 className="font-semibold">homepage</h1>
-          <h1 className="opacity-60">homepage-amber-five.vercel.app</h1>
+          <h1 className="font-semibold">{title}</h1>
+          <h1 className="opacity-60">{source}</h1>
         </div>
       </div>
       <div className="opacity-60 mt-3">
-        <h1>Fixed some details</h1>
+        <h1>{commit}</h1>
         <div className="inline-flex items-center gap-1 text-sm">
           <GitBranch size={16} />
-          From main
+          From {branch}
         </div>
       </div>
       <div>
--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Card from './components/Card'
 import SearchForm from './components/SearchForm'
+import avatar from '../public/avatar.jpg'
 
 export default function Home() {
   return (
@@ -7,9 +8,24 @@ export default function Home() {
       <div className="max-w-7xl p-4 mx-auto">
         <SearchForm />
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <Card />
-          <Card />
-          <Card />
+          <Card
+            image={avatar}
+            title="homepage"
+            source="homepage-amber-five.vercel.app"
+            commit="Fixed some details"
+          />
+          <Card
+            image={avatar}
+            title="homepage"
+            source="homepage-amber-five.vercel.app"
+            commit="Fixed some details"
+          />
+          <Card
+            image={avatar}
+            title="homepage"
+            source="homepage-amber-five.vercel.app"
+            commit="Fixed some details"
+          />
         </div>
       </div>
     </div>
